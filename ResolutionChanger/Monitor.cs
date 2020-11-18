9e00313e200e6808c459dd07d571469049d2a642
tyr0wl <paul.ch87@gmail.com>
2020-11-18 16:55:27 +0100

    Update monitor ToString method to be more descriptive

--- ResolutionChanger/Monitor.cs
+++ ResolutionChanger/Monitor.cs
@@ -7,5 +7,12 @@ namespace ResolutionChanger
         public string DeviceName { get; set; }
         public bool IsPrimary { get; set; }
         public bool IsActive { get; set; }
+
+        public override string ToString()
+        {
+            var primaryString = IsPrimary ? "P" : null;
+            var activeString = IsActive ? "A" : "D";
+            return $"{DeviceName}({primaryString}{activeString})";
+        }
     }
 }
