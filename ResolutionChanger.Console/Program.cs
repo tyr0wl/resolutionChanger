6a5ed98589ff5347dc339783835f95903b201483
tyr0wl <paul.ch87@gmail.com>
2020-11-18 16:40:43 +0100

    Add project

---
+++ ResolutionChanger.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using ResolutionChanger.Unmanaged;
+using SystemConsole = System.Console;
+
+namespace ResolutionChanger.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
