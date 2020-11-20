1643e0078a56ee7c2ddb269a814e93d6de7ed66b
tyr0wl <paul.ch87@gmail.com>
2020-11-20 14:38:00 +0100

    Refactor SourceMode and DisplayConfigPixelFormat to follow naming rules and include documentation

--- ResolutionChanger/Win32/DisplayConfig/DisplayConfigPixelFormat.cs
+++ ResolutionChanger/Win32/DisplayConfig/DisplayConfigPixelFormat.cs
@@ -1,13 +1,45 @@
 namespace ResolutionChanger.Win32.DisplayConfig
 {
+    /// <summary>
+    ///     The DISPLAYCONFIG_PIXELFORMAT enumeration specifies pixel format in various bits per pixel (BPP) values.
+    ///     DISPLAYCONFIG_PIXELFORMAT enumeration (wingdi.h)
+    /// </summary>
     public enum DisplayConfigPixelFormat : uint
     {
         None = 0,
 
-        DISPLAYCONFIG_PIXELFORMAT_8BPP = 1,
-        DISPLAYCONFIG_PIXELFORMAT_16BPP = 2,
-        DISPLAYCONFIG_PIXELFORMAT_24BPP = 3,
-        DISPLAYCONFIG_PIXELFORMAT_32BPP = 4,
-        DISPLAYCONFIG_PIXELFORMAT_NONGDI = 5,
+        /// <summary>
+        ///     Indicates 8 BPP format.
+        ///     DISPLAYCONFIG_PIXELFORMAT_8BPP
+        /// </summary>
+        PixelFormat8Bpp = 1,
+
+        /// <summary>
+        ///     Indicates 16 BPP format.
+        ///     DISPLAYCONFIG_PIXELFORMAT_16BPP
+        /// </summary>
+        PixelFormat16Bpp = 2,
+
+        /// <summary>
+        ///     Indicates 24 BPP format.
+        ///     DISPLAYCONFIG_PIXELFORMAT_24BPP
+        /// </summary>
+        PixelFormat24Bpp = 3,
+
+        /// <summary>
+        ///     Indicates 32 BPP format.
+        ///     DISPLAYCONFIG_PIXELFORMAT_32BPP
+        /// </summary>
+        PixelFormat32Bpp = 4,
+
+        /// <summary>
+        ///     Indicates that the current display is not an 8, 16, 24, or 32 BPP GDI desktop mode. For example, a call to the
+        ///     <see cref="DisplayConfigApi.QueryDisplayConfig" /> function returns <see cref="PixelFormatNonGdi" /> if a DirectX
+        ///     application previously set the desktop to A2R10G10B10 format. A call to the
+        ///     <see cref="DisplayConfigApi.SetDisplayConfig" /> function fails if any pixel formats for active paths are set to
+        ///     <see cref="PixelFormatNonGdi" />.
+        ///     DISPLAYCONFIG_PIXELFORMAT_NONGDI
+        /// </summary>
+        PixelFormatNonGdi = 5
     }
 }
--- ResolutionChanger/Win32/DisplayConfig/SourceMode.cs
+++ ResolutionChanger/Win32/DisplayConfig/SourceMode.cs
@@ -2,12 +2,31 @@ using System.Runtime.InteropServices;
 
 namespace ResolutionChanger.Win32.DisplayConfig
 {
+    /// <summary>
+    ///     The DISPLAYCONFIG_SOURCE_MODE structure represents a point or an offset in a two-dimensional space.
+    ///     DISPLAYCONFIG_SOURCE_MODE structure (wingdi.h)
+    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     public struct SourceMode
     {
-        public uint width;
-        public uint height;
-        public DisplayConfigPixelFormat pixelFormat;
-        public PointL position;
+        /// <summary>
+        ///     The width in pixels of the source mode.
+        /// </summary>
+        [MarshalAs(UnmanagedType.U4)] public uint width;
+
+        /// <summary>
+        ///     The height in pixels of the source mode.
+        /// </summary>
+        [MarshalAs(UnmanagedType.U4)] public uint height;
+
+        /// <summary>
+        ///     A value from the DISPLAYCONFIG_PIXELFORMAT enumeration that specifies the pixel format of the source mode.
+        /// </summary>
+        [MarshalAs(UnmanagedType.U4)] public DisplayConfigPixelFormat pixelFormat;
+
+        /// <summary>
+        ///     A POINTL structure that specifies the position in the desktop coordinate space of the upper-left corner of this source surface. The source surface that is located at (0, 0) is always the primary source surface.
+        /// </summary>
+        [MarshalAs(UnmanagedType.Struct)] public PointL position;
     }
 }
