9829114350972acfcdb5cdf1676d4fcf8467edc5
tyr0wl <paul.ch87@gmail.com>
2020-12-04 05:21:38 +0100

    Remove readonly modifier from DesktopImageInfo

--- ResolutionChanger/Win32/DisplayConfig/ModeInfo/DesktopImageInfo.cs
+++ ResolutionChanger/Win32/DisplayConfig/ModeInfo/DesktopImageInfo.cs
@@ -7,21 +7,21 @@ namespace ResolutionChanger.Win32.DisplayConfig.ModeInfo
     ///     DISPLAYCONFIG_DESKTOP_IMAGE_INFO structure (wingdi.h)
     /// </summary>
     [StructLayout(LayoutKind.Sequential)]
-    public readonly struct DesktopImageInfo
+    public struct DesktopImageInfo
     {
         /// <summary>
         ///     A POINTL structure that specifies the size of the VidPn source surface that is being displayed on the monitor.
         /// </summary>
-        [MarshalAs(UnmanagedType.Struct)] public readonly PointL PathSourceSize;
+        [MarshalAs(UnmanagedType.Struct)] public PointL PathSourceSize;
 
         /// <summary>
         ///     A RECTL structure that defines where the desktop image will be positioned within path source. Region must be completely inside the bounds of the path source size.
         /// </summary>
-        [MarshalAs(UnmanagedType.Struct)] public readonly RectangleL DesktopImageRegion;
+        [MarshalAs(UnmanagedType.Struct)] public RectangleL DesktopImageRegion;
 
         /// <summary>
         ///     A RECTL structure that defines which part of the desktop image for this clone group will be displayed on this path. This currently must be set to the desktop size.
         /// </summary>
-        [MarshalAs(UnmanagedType.Struct)] public readonly RectangleL DesktopImageClip;
+        [MarshalAs(UnmanagedType.Struct)] public RectangleL DesktopImageClip;
     }
 }
