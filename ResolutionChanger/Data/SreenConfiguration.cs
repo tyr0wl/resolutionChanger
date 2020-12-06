3c41b40b9fec4bff2b391acc50496d2a4efc4980
tyr0wl <paul.ch87@gmail.com>
2020-12-06 05:30:50 +0100

    Add ScreenConfiguration class which is intended to store paths and modes for a given screen configuration

---
+++ ResolutionChanger/Data/SreenConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ResolutionChanger.Data.Modes;
+using ResolutionChanger.Data.Paths;
+
+namespace ResolutionChanger.Data
+{
+    public class ScreenConfiguration
+    {
+        public string Name { get; init; }
+        public List<ScreenPath> Paths { get; init; } = new();
+        public List<ScreenMode> Modes { get; init; } = new();
+
+        public ScreenConfiguration Copy
+        (
+            string name = null,
+            List<ScreenPath> paths = null,
+            List<ScreenMode> modes = null
+        )
+        {
+            return new()
+            {
+                Name = name ?? Name,
+                Paths = paths ?? Paths,
+                Modes = modes ?? Modes,
+            };
+        }
+    }
+}
